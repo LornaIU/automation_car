6fb38965c08022e5ff9a5e1f9fb2d49bcccdb7ab
Lorna <70303875+LornaIU@users.noreply.github.com>
2021-10-07 13:54:41 +0800

    Update auto_car.py

--- auto_car.py
+++ auto_car.py
@@ -2,10 +2,10 @@ import cv2
 import autocar_module as m
 import numpy as np
 
-capture = cv2.VideoCapture('road.mp4')      # 建立 VideoCapture 物件
+capture = cv2.VideoCapture('road.mp4')     
 if capture.isOpened():
     while True:
-        sucess, img = capture.read()            # 讀取影像
+        sucess, img = capture.read()    
         if sucess:
             edge = m.get_edge(img)                   # 邊緣偵測
             roi = m.get_roi(edge)                   # 取得 ROI
@@ -19,12 +19,12 @@ if capture.isOpened():
             if avglines is not None:
                 lines = m.get_sublines(img, avglines) # 取得要畫出的左右 2 條線段
                 img = m.draw_lines(img, lines)      # 畫出線段
-            cv2.imshow('Frame', img)      # 顯示影像
+            cv2.imshow('Frame', img)     
         k = cv2.waitKey(1)                # 等待按鍵輸入
         if k == ord('q') or k == ord('Q'):  # 按下 Q(q) 結束迴圈
             print('exit')
-            cv2.destroyAllWindows()         # 關閉視窗
-            capture.release()               # 關閉攝影機
+            cv2.destroyAllWindows()         
+            capture.release()              
             break
 else:
     print('開啟攝影機失敗')
